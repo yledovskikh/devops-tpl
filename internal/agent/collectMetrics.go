b63471f401f76b676eebc1995b0db1a7f7884e07
Yury <y.ledovskikh@gmail.com>
2022-04-24 15:19:15 +0700

    refactoring agent

---
+++ internal/agent/collectMetrics.go
@@ -0,0 +1,126 @@
+package agent
+
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+	"os"
+	"os/signal"
+	"runtime"
+	"strconv"
+	"syscall"
+	"time"
+)
+
+type metric struct {
+	name       string
+	metricType string
+	value      string
+}
+
+func collectMetrics(rtm runtime.MemStats, pollCount int64, randomValue float64) []metric {
+	m := []metric{
+		{"Alloc", "gauge", strconv.FormatUint(rtm.Alloc, 10)},
+		{"BuckHashSys", "gauge", strconv.FormatUint(rtm.BuckHashSys, 10)},
+		{"Frees", "gauge", strconv.FormatUint(rtm.Frees, 10)},
+		{"GCCPUFraction", "gauge", strconv.FormatFloat(rtm.GCCPUFraction, 'f', -1, 64)},
+		{"GCSys", "gauge", strconv.FormatUint(rtm.GCSys, 10)},
+		{"HeapAlloc", "gauge", strconv.FormatUint(rtm.HeapAlloc, 10)},
+		{"HeapIdle", "gauge", strconv.FormatUint(rtm.HeapIdle, 10)},
+		{"HeapInuse", "gauge", strconv.FormatUint(rtm.HeapInuse, 10)},
+		{"HeapObjects", "gauge", strconv.FormatUint(rtm.HeapObjects, 10)},
+		{"HeapReleased", "gauge", strconv.FormatUint(rtm.HeapReleased, 10)},
+		{"HeapSys", "gauge", strconv.FormatUint(rtm.HeapSys, 10)},
+		{"LastGC", "gauge", strconv.FormatUint(rtm.LastGC, 10)},
+		{"Lookups", "gauge", strconv.FormatUint(rtm.Lookups, 10)},
+		{"MCacheInuse", "gauge", strconv.FormatUint(rtm.MCacheInuse, 10)},
+		{"MCacheSys", "gauge", strconv.FormatUint(rtm.MCacheSys, 10)},
+		{"MSpanInuse", "gauge", strconv.FormatUint(rtm.MSpanInuse, 10)},
+		{"MSpanSys", "gauge", strconv.FormatUint(rtm.MSpanSys, 10)},
+		{"Mallocs", "gauge", strconv.FormatUint(rtm.Mallocs, 10)},
+		{"NextGC", "gauge", strconv.FormatUint(rtm.NextGC, 10)},
+		{"NumForcedGC", "gauge", strconv.FormatUint(uint64(rtm.NumForcedGC), 10)},
+		{"NumGC", "gauge", strconv.FormatUint(uint64(rtm.NumGC), 10)},
+		{"OtherSys", "gauge", strconv.FormatUint(rtm.OtherSys, 10)},
+		{"PauseTotalNs", "gauge", strconv.FormatUint(rtm.PauseTotalNs, 10)},
+		{"StackInuse", "gauge", strconv.FormatUint(rtm.StackInuse, 10)},
+		{"StackSys", "gauge", strconv.FormatUint(rtm.StackSys, 10)},
+		{"Sys", "gauge", strconv.FormatUint(rtm.Sys, 10)},
+		{"TotalAlloc", "gauge", strconv.FormatUint(rtm.TotalAlloc, 10)},
+		//Custom metrics
+		{"PollCount", "counter", strconv.FormatInt(pollCount, 10)},
+		{"RandomValue", "gauge", strconv.FormatFloat(randomValue, 'f', -1, 64)},
+	}
+	return m
+}
+
+func postMetrics(m []metric, endpoint string, contextURL string) {
+	fmt.Println(time.Now().Format(time.UnixDate), "Push metrics:")
+	for _, value := range m {
+		url := endpoint + "/" + contextURL + "/" + value.metricType + "/" + value.name + "/" + value.value
+		//fmt.Println(url)
+		request, err := http.NewRequest(http.MethodPost, url, nil)
+		request.Header.Set("Content-Type", "text/plain")
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		client := &http.Client{}
+		response, err := client.Do(request)
+		if err != nil {
+			fmt.Println(err.Error())
+			os.Exit(1)
+
+		}
+		err = response.Body.Close()
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+
+}
+
+func RefreshMetrics(pollInterval time.Duration, reportInterval time.Duration, endpoint string, contextURL string) {
+	var m []metric
+	var rtm runtime.MemStats
+	var pollCount int64 = 0
+	pollIntervalTicker := time.NewTicker(pollInterval)
+	reportIntervalTicker := time.NewTicker(reportInterval)
+	rand.Seed(time.Now().UnixNano())
+	for {
+		select {
+		case <-pollIntervalTicker.C:
+			r := rand.Float64()
+			pollCount++
+			runtime.ReadMemStats(&rtm)
+			m = collectMetrics(rtm, 1, r)
+			fmt.Println(time.Now().Format(time.UnixDate), "Counter update metrics: ", pollCount)
+		case <-reportIntervalTicker.C:
+			postMetrics(m, endpoint, contextURL)
+		}
+	}
+}
+
+func TerminateAgent(exitChan chan int) {
+	signalChannel := make(chan os.Signal, 1)
+	signal.Notify(signalChannel,
+		syscall.SIGTERM,
+		syscall.SIGINT,
+		syscall.SIGQUIT)
+	for {
+		sig := <-signalChannel
+		switch sig {
+		case syscall.SIGTERM:
+			fmt.Println("sigterm")
+			exitChan <- 0
+		case syscall.SIGINT:
+			fmt.Println("sigint")
+			exitChan <- 0
+		case syscall.SIGQUIT:
+			fmt.Println("sigquit")
+			exitChan <- 0
+		default:
+			fmt.Println("Unknown signal.")
+			exitChan <- 1
+		}
+	}
+}
---
+++ internal/handlers/handlers_test.go
@@ -0,0 +1,161 @@
+package handlers
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func testRequest(t *testing.T, ts *httptest.Server, method, path string) (*http.Response, string) {
+	req, err := http.NewRequest(method, ts.URL+path, nil)
+	require.NoError(t, err)
+
+	resp, err := http.DefaultClient.Do(req)
+	require.NoError(t, err)
+
+	respBody, err := ioutil.ReadAll(resp.Body)
+	require.NoError(t, err)
+
+	defer resp.Body.Close()
+
+	return resp, string(respBody)
+}
+
+//func TestGetMetric(t *testing.T) {
+//	//type args struct {
+//	//	w http.ResponseWriter
+//	//	r *http.Request
+//	//}
+//	type want struct {
+//		code int
+//		//response string
+//		//contentType string
+//	}
+//	type metric struct {
+//		metricType  string
+//		metricName  string
+//		metricValue string
+//	}
+//	tests := []struct {
+//		name   string
+//		metric metric
+//		want   want
+//	}{
+//		{
+//			name:   "simple test #1",
+//			metric: metric{"goage", "metric1", "111.111"},
+//			want:   want{200},
+//		},
+//	}
+//	for _, tt := range tests {
+//		t.Run(tt.name, func(t *testing.T) {
+//
+//			r := chi.NewRouter()
+//			ts := httptest.NewServer(r)
+//			defer ts.Close()
+//			path := "/update/{metricType}/{metricName}/{metricValue}"
+//			response, _ := testRequest(t, ts, "POST", path)
+//
+//			if response.StatusCode != 200 {
+//				t.Fatal("err")
+//			}
+//
+//			//assert.Equal(t, http.StatusOK, resp.StatusCode)
+//			//assert.Equal(t, "brand:renault", body)
+//
+//			//
+//			//
+//			//
+//			//m := tt.metric
+//			////path := fmt.Sprintf("/update/%s/%s/%s",
+//			////	m.metricType, m.metricName, m.metricValue)
+//			//req, err := http.NewRequest("POST", path, nil)
+//			//if err != nil {
+//			//	t.Fatal(err)
+//			//}
+//			//rr := httptest.NewRecorder()
+//			//router := mux.NewRouter()
+//			//router.HandleFunc("/update/{metricType}/{metricName}/{metricValue}", handlers.UpdateMeticHandler)
+//			//router.ServeHTTP(rr, req)
+//			//res := rr.Result()
+//			//
+//			//if res.StatusCode != tc.want.code {
+//			//	t.Errorf("Expected status code %d, got %d", tc.want.code, rr.Code)
+//			//}
+//			//
+//			//defer res.Body.Close()
+//			//resBody, err := io.ReadAll(res.Body)
+//			//if err != nil {
+//			//	t.Fatal(err)
+//			//}
+//			//if string(resBody) != tc.want.response {
+//			//	t.Errorf("Expected body %s, got %s", tc.want.response, rr.Body.String())
+//			//}
+//		})
+//	}
+//}
+
+func TestPostMetric(t *testing.T) {
+	type want struct {
+		statusCode int
+		//response string
+		//contentType string
+	}
+	type metric struct {
+		metricType  string
+		metricName  string
+		metricValue string
+	}
+	tests := []struct {
+		name   string
+		metric metric
+		want   want
+	}{
+		{
+			name:   "simple test upload metric gauge",
+			metric: metric{"gauge", "testm1", "1111.1"},
+			want:   want{200},
+		},
+		{
+			name:   "incorrect upload metric gauge",
+			metric: metric{"gauge", "testm1", "1.111.1"},
+			want:   want{400},
+		},
+		{
+			name:   "simple test upload metric counter",
+			metric: metric{"counter", "testm1", "1"},
+			want:   want{200},
+		},
+		{
+			name:   "incorrect upload metric counter",
+			metric: metric{"counter", "testm1", "1.1"},
+			want:   want{400},
+		},
+		{
+			name:   "incorrect metric type",
+			metric: metric{"incorrect", "testm1", "1.1"},
+			want:   want{501},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			path := "/update/" + tt.metric.metricType + "/" + tt.metric.metricName + "/" + tt.metric.metricValue
+			req, err := http.NewRequest("POST", path, nil)
+			if err != nil {
+				t.Fatal(err)
+			}
+			tr := httptest.NewRecorder()
+			r := chi.NewRouter()
+			r.HandleFunc("/update/{metricType}/{metricName}/{metricValue}", PostMetric)
+			r.ServeHTTP(tr, req)
+			res := tr.Result()
+			assert.Equal(t, tt.want.statusCode, res.StatusCode)
+
+		})
+	}
+}
