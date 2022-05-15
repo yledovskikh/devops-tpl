33e2cce402deb49cf35eb6a7e78a17a655afd383
Yury <y.ledovskikh@gmail.com>
2022-05-15 15:21:59 +0700

    fix: pass test

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -53,7 +53,7 @@ func (s *Server) UpdateJSONMetric(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(status)
 	}
 
-	w.WriteHeader(http.StatusCreated)
+	//w.WriteHeader(http.StatusCreated)
 	w.Header().Set("Content-Type", "application/json")
 	resp := make(map[string]string)
 	resp["message"] = "Status Created"
