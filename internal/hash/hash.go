4f8b6a0c6c10e4ca75a6d41302d5f9ca11eedeb8
Yury <y.ledovskikh@gmail.com>
2022-05-27 18:56:56 +0700

    fix:1

--- internal/hash/hash.go
+++ internal/hash/hash.go
@@ -10,7 +10,7 @@ func SignData(key, data string) string {
 	h := hmac.New(sha256.New, []byte(key))
 	h.Write([]byte(data))
 	sign := h.Sum(nil)
-	return hex.EncodeToString(sign)
+	return hex.EncodeToString(sign[:])
 }
 
 func VerifyHash(hash1, hash2 string) bool {
